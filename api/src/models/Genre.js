db1d0464d46b24dd472db195181341c578e6d916
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2023-03-25 01:36:10 -0300

    Configurando la base de datos y definición de modelos

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -3,6 +3,10 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("Genre", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,9 +3,30 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("Videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    platforms: {
+      type: DataTypes.ARRAY(DataTypes.INTEGER),
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
     },
   });
 };
