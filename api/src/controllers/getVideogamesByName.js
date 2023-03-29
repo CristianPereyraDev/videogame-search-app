3775808c3a065c6ae840f0600c183cccbc6fcd7e
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2023-03-28 23:20:46 -0300

    Actualización del controlador getVideogamesByName en el Back-end

--- api/src/controllers/getVideogamesByName.js
+++ api/src/controllers/getVideogamesByName.js
@@ -1,27 +1,52 @@
 const { Op } = require("sequelize");
 const { Videogame, Genre } = require("../db");
 const { getGamesFromApi } = require("../utils/api.util");
+const { GAMES_PAGE_SIZE } = require("../configs/api.configs");
 
 async function getVideogamesByName(req, res) {
   try {
     const { name, page, pageSize } = req.query;
-    // Get from db with a limit.
+    // Traigo los primeros 100 juegos de la api
+    const gamesFromApi = await getGamesFromApi(GAMES_PAGE_SIZE, name);
+    // Traigo todos los juegos de la base de datos que cumplan la condición
     const gamesFromDB = await Videogame.findAll({
       where: { name: { [Op.like]: `%${name}%` } },
-      limit: pageSize,
     });
-    // Get from api the remainig instances.
-    const gamesFromApi = await getGamesFromApi(
-      null,
-      Math.max(0, pageSize - gamesFromDB.length),
-      name
-    );
-    const results = [...gamesFromDB, ...gamesFromApi];
-    // Get next page
-    const nextPage = `videogames/name?name=${name}`;
-    if (result.length > 0)
-      res.status(200).json({ nextPage: nextPage, results: results });
-    else
+    // Uno todos los juegos
+    const gamesFromApiAndDB = [...gamesFromDB, ...gamesFromApi];
+    if (gamesFromApiAndDB.length > 0) {
+      // Pagination
+      const maxPages = Math.ceil(gamesFromApiAndDB.length / pageSize);
+      if (page >= 1 && page <= maxPages) {
+        const startPage = pageSize * (page - 1);
+        const endPage = pageSize * page;
+        const paginatedVideogames = gamesFromApiAndDB.slice(startPage, endPage);
+        const nextPageUrl =
+          page < maxPages
+            ? `${req.protocol}://${req.get(
+                "host"
+              )}/videogames/name?name=${name}&page=${
+                Number(page) + 1
+              }&pageSize=${pageSize}`
+            : null;
+        const prevPageUrl =
+          page > 1
+            ? `${req.protocol}://${req.get(
+                "host"
+              )}/videogames/name?name=${name}&page=${
+                page - 1
+              }&pageSize=${pageSize}`
+            : null;
+        // Response
+        res.status(200).json({
+          prevPage: prevPageUrl,
+          nextPage: nextPageUrl,
+          results: paginatedVideogames,
+        });
+      } else {
+        res.status(400).json({ message: "Error de paginación." });
+      }
+    } else
       res
         .status(400)
         .json({ message: "No hay juegos que coincidan con esa palabra." });
