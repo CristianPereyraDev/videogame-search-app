c7633e4b5813cbd90467cb3462ee89d7c19fd815
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2023-04-06 01:43:53 -0300

    Avances en el componente Detail

--- api/src/controllers/getVideogameById.js
+++ api/src/controllers/getVideogameById.js
@@ -3,10 +3,27 @@ const { getGameByIdFromApi } = require("../utils/api.util");
 
 async function getVideogameById(req, res) {
   try {
-    console.log("getVideogameById");
+    // Primero busco en la api
     const { idVideogame } = req.params;
-    let game = await getGameByIdFromApi(idVideogame);
-    res.status(200).json(game);
+    const { isdb } = req.query;
+    console.log("isdb", isdb);
+    let videogame = {};
+    if (isdb === "true" && idVideogame) {
+      videogame = await Videogame.findByPk(idVideogame);
+    } else {
+      let response = await getGameByIdFromApi(idVideogame);
+      videogame = {
+        id: response.id,
+        name: response.name,
+        description: response.description,
+        platforms: response.platforms,
+        image: response.background_image,
+        released: response.released,
+        rating: response.rating,
+      };
+    }
+    if (videogame) res.status(200).json(videogame);
+    else res.status(400).json({ message: "El videojuego no existe" });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,9 +1,52 @@
+import { Link, useParams } from "react-router-dom";
 import styles from "./Detail.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Detail(props) {
+  const { gameId } = useParams();
+  const [videogame, setVideogame] = useState({});
+
+  useEffect(() => {
+    async function getVideogameDetail() {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/videogames/${gameId}`
+        );
+        if (response.data.name) {
+          setVideogame(response.data);
+        } else {
+          window.alert("No existe el videogame");
+        }
+      } catch (error) {
+        setVideogame({});
+        window.alert(error.message);
+      }
+    }
+    getVideogameDetail();
+  }, [gameId]);
+
   return (
     <div>
-      <h1>Detail Page</h1>
+      <Link to="/home">
+        <button>Go Back</button>
+      </Link>
+      <div className={styles.detailContainer}>
+        {/* Render name */}
+        <h3>{videogame.name}</h3>
+        {/* Render image */}
+        <img src={videogame.image} alt={videogame.name} />
+        {/* Render description, la cual puede venir como texto html("<p>Description text</p>") */}
+        <div dangerouslySetInnerHTML={{ __html: videogame.description }} />
+        {/* Render platforms */}
+        <div>{/*videogame.platforms.map((platform) => "")*/}</div>
+        {/* Render rating */}
+        <div>Rating: {videogame.rating}</div>
+        {/* Render released */}
+        <div>Released: {videogame.released}</div>
+        {/* Render genres */}
+        <div>Genres:</div>
+      </div>
     </div>
   );
 }
