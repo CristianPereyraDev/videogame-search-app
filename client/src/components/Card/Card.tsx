6111a3f43ff164c13039a8b9a6af26ecaa3d5895
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2023-09-12 13:11:20 -0300

    Added default image that will be used in broken links

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -8,15 +8,17 @@ import {
   Typography,
 } from '@mui/material';
 import { IGame } from '../../features/games/types';
+import images from '../../Images/Images';
 
 export default function GameCard({ game }: { game: IGame }) {
   return (
-    <Card sx={{ maxWidth: 340 }}>
+    <Card sx={{ maxWidth: 340, minWidth: 340 }}>
       <CardMedia
         component='img'
         height={194}
-        image={game.image}
+        image={game.image || images.default}
         alt={game.name}
+        sx={{ objectFit: game.image ? 'cover' : 'scale-down' }}
       />
       <CardContent>
         <Stack
@@ -24,23 +26,33 @@ export default function GameCard({ game }: { game: IGame }) {
           justifyContent='space-between'
           alignItems='center'
         >
-          <Typography variant='subtitle1'>{game.name}</Typography>
-          <Typography variant='body2'>
+          <Typography variant='h6'>{game.name}</Typography>
+          <Typography variant='body2' color='text.secondary'>
             Rating: {game.rating.toString()}
           </Typography>
         </Stack>
-        <Typography variant='body2'>{game.released}</Typography>
-        <Typography variant='body2'>
-          {game.genres.map((genre, i) => (
-            <span key={genre.id}>
-              {i !== 0 ? ', ' : ''}
-              {genre.name}
-            </span>
-          ))}
-        </Typography>
+        <Stack direction='row' justifyContent='space-between'>
+          <Typography variant='body2' color='text.secondary'>
+            Released:
+          </Typography>
+          <Typography variant='body2'>{game.released}</Typography>
+        </Stack>
+        <Stack direction='row' justifyContent='space-between'>
+          <Typography variant='body2' color='text.secondary'>
+            Genres:
+          </Typography>
+          <Typography variant='body2'>
+            {game.genres.map((genre, i) => (
+              <span key={genre.id}>
+                {i !== 0 ? ', ' : ''}
+                {genre.name}
+              </span>
+            ))}
+          </Typography>
+        </Stack>
       </CardContent>
       <CardActions>
-        <Button href={`/details/${game.id}`} variant='contained'>
+        <Button href={`/details/${game.id}`} variant='contained' size='small'>
           Details
         </Button>
       </CardActions>
